78aedf202c1b670ce3caad74d5bf26b46c2718f5
Daniel Modig <daniel@ikoncept.se>
2016-04-09 22:58:01 +0200

    Replaced all sneaky tabs with dualspaces

--- templates/main.php
+++ templates/main.php
@@ -5,40 +5,40 @@
   <!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
   <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
   <head>
-  	<meta charset="utf-8">
-  	<!--[if IE]><![endif]-->
-  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  	<title><?php echo AUTH_REALM ?></title>
-  	<meta name="description" content="">
-  	<meta name="keywords" content="" />
-  	<meta name="author" content="">
-  	<meta name="viewport" content="initial-scale=1.0">
-  	<!-- !CSS -->
-  	<link rel="stylesheet" href="assets/css/style.css?v=1">
-  	
+    <meta charset="utf-8">
+    <!--[if IE]><![endif]-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo AUTH_REALM ?></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="" />
+    <meta name="author" content="">
+    <meta name="viewport" content="initial-scale=1.0">
+    <!-- !CSS -->
+    <link rel="stylesheet" href="assets/css/style.css?v=1">
+    
   </head>
   
   <!-- !Body -->
   <body>
-  	<div id="container">
-  		<header>
-    		<h1><?php echo AUTH_REALM ?></h1>
-  		</header><!-- /header -->
-  		
-  		<section id="main">
-  		  <?php
-    		  $presentations = new Presentations();
-    		  
+    <div id="container">
+      <header>
+        <h1><?php echo AUTH_REALM ?></h1>
+      </header><!-- /header -->
+      
+      <section id="main">
+        <?php
+          $presentations = new Presentations();
+          
           foreach($presentations->get_presentations() as $p):
             require 'templates/presentation.php';
           endforeach;
-    		?>
-  		</section><!-- /main -->
-  		
-  		<footer>
-  		
-  		</footer><!-- /footer -->
-  	</div><!--!/#container -->
-  	
+        ?>
+      </section><!-- /main -->
+      
+      <footer>
+      
+      </footer><!-- /footer -->
+    </div><!--!/#container -->
+    
   </body>
 </html>
