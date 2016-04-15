69bdccfed23866b95ce971e63eb151706a9dc80b
foag <daniel@ikoncept.se>
2016-04-15 22:39:21 +0200

    Added config.php.example to prevent overwriting default config

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
   
   /*
-    MySQL:
+    MySQL som förutsätts:
      
     CREATE DATABASE `my_slack_db` DEFAULT CHARACTER SET = `utf8` DEFAULT COLLATE = `utf8_general_ci`;
     
-    USE `my_slack_db`;
+    USE 'my_slack_db';
     
     CREATE TABLE `intro_post` (
       `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -20,61 +20,57 @@
       `user_name` varchar(36) DEFAULT NULL,
       `text` varchar(4000) DEFAULT NULL,
       `trigger_word` varchar(24) DEFAULT NULL,
-      `user_avatar` varchar(256) DEFAULT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
   */
 
   /* 
-    Define users as 'user' => 'password';    
-    To disable authentication, simply comment out define('AUTH_USERS')
+    Definiera användare som 'användare' => 'lösenord';    
+    För att inte använda autentisering, kommentera bort definitionen "AUTH_USERS"
   */ 
   require_once 'class/auth.php';   
-  $users = array(
-    "user" => "pass",
-    "user2" => "pass"
-  );
+  $users = array("rick" => "astley");
   define('AUTH_USERS',auth::serialized_users($users));
-  //Define realm name
-  define('AUTH_REALM','NAME OF REALM');
+  //Definiera namn på sidan
+  define('AUTH_REALM','&lt;F/NHACKAT&gt;');  
   
   /* 
-    Define DB credentials
+    Definiera uppgifter för databas-anslutning
   */   
-  define('DBUSER','');
-  define('DBPASS','');
-  define('DBHOST','');
-  define('DBNAME','');
+  define('DBUSER','finhack');
+  define('DBPASS','$t34MYf3Et+399');
+  define('DBHOST','localhost');  
+  define('DBNAME','finhack');  
   
   /* 
-    Define Slack token per channel and Slack team to validate the call, Slack API token to extract user avatars
+    Definiera Slack-token och Slack-team
   */   
   define('SL_TOKEN',json_encode(array(
-    'channel1' => 'XXXXXXXXXXXXXXXXXX',
-    'channel2' => 'XXXXXXXXXXXXXXXXXX',
+    'introductions' => 'a9JKvsbr9rm5fwilFsVnZ1It',
+    'jobb' => 'vQHtuG71hdRy9F8tAyIqjbNS'
   )));
-  define('SL_TEAM','');
-  define('SL_API_TOKEN','');
+  define('SL_TEAM','T0G7KTQ68');
+  define('SL_API_TOKEN','xoxp-16257942212-32953202976-33412846918-0558901562');
   
   /*
-    Define response messages for the Slack bot to display in the channel.
-    Empty messages are simply ignored
+    Definiera response-texter för Slacks bot att visa i kanalen.
+    Om ingen text anges resulterar det i att Slack ignorerar. 
   */
-  //Message saved
+  //Svar om meddelandet sparats. 
   define('RESPONSES',json_encode(array(
-    'channel1' => "Response for saved post to channel 1",
-    'channel2' => "Response for saved post to channel 2"
+    'introductions' => "Tack så mycket! Din introduktion sparad för framtiden :). För att titta på din och andras presentationer, gå till http://apps.icatserver.com/finhackat/?channel=introductions \nAnv: rick \nPass: astley",
+    'jobb' => "Tack för tipset, annonsen finns nu på http://apps.icatserver.com/finhackat/?channel=jobb \nAnv: rick \nPass: astley"
   )));
-  //Message save failed
-  define('RESPONSE_SAVED_FAIL',''); 
-  //Error in Slack call (SL_TOKEN, SL_TEAM)
-  define('RESPONSE_CALL_ERROR','');
-  //Error in authentication
-  define('RESPONSE_AUTH_ERROR','');
+  //Svar om meddelandet inte sparats.
+  define('RESPONSE_SAVED_FAIL','Något gick helt åt pipan. Säg till @foag.'); 
+  //Svar om anropet innehåller fel enligt SL_TOKEN eller SL_TEAM.
+  define('RESPONSE_CALL_ERROR','Felaktigt anrop från Slack');
+  //Svar om AUTH_USERS ger fel anv/lösenord
+  define('RESPONSE_AUTH_ERROR','Fel uppgifter, försök inte luras!');
   
   /*
-    Define testdata, data below is a dummy-post
-    Uncomment to test without having to spam the channel
+    Definiera testdata (JSON från en utgående webhook-POST från Slack.
+    Avkommentera för att testa utan att spamma kanaler som påverkas.
   */
-  $testpost = '{"token":"a9JKvsbr9rm5fwilFsVnZ1It","team_id":"TXXXXXXX","team_domain":"test","service_id":"48618487348","channel_id":"C9S3ABCDE","channel_name":"introductions","timestamp":"1460226325.000157","user_id":"UXXXXXXXX","user_name":"rick_astley","text":"Never gonna give you up, never gonna let you down, never gonna run around and desert you","trigger_word":"intro"}';
+  $testpost = '{"token":"a9JKvsbr9rm5fwilFsVnZ1It","team_id":"T0G7KTQ68","team_domain":"finhackat","service_id":"33218487348","channel_id":"C0S3HCBEU","channel_name":"introductions","timestamp":"1460226325.000157","user_id":"U0YU15YUQ","user_name":"foag","text":"intro testar slimmad kod","trigger_word":"intro"}';
   //define('TESTPOST',$testpost);
